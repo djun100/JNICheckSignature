fae832263ab8bdec7ad8dd8484ad36dfc74a0da8
wangxuechao <djun100@qq.com>
2019-04-06 18:39:22 +0800

    use cmake

---
+++ app/src/main/java/com/example/hellojni/HelloJni.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.hellojni;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.text.TextUtils;
+
+public class HelloJni extends Activity {
+
+	static {
+		System.loadLibrary("native-lib");
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getSignature(this, null);
+		// JNI检查
+		 jniCheckAPP();
+	}
+
+	public void popAlarm() {
+		Dialog alertDialog = new AlertDialog.Builder(this).
+		// setTitle("对话框的标题").
+				setMessage("签名认证失败，请从官方渠道下载游戏哦").setPositiveButton("确定", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO Auto-generated method stub
+						System.exit(0);
+					}
+				}).create();
+		alertDialog.show();
+	}
+
+	public native void jniCheckAPP();
+
+	/**
+	 * @param context
+	 * @param pkgname
+	 *            null->self
+	 */
+	public String getSignature(Context context, String pkgname) {
+		pkgname = TextUtils.isEmpty(pkgname) ? context.getPackageName() : pkgname;
+		PackageManager pm = context.getPackageManager();
+		try {
+			PackageInfo packageInfo = pm.getPackageInfo(pkgname, PackageManager.GET_SIGNATURES);
+			String signatures = packageInfo.signatures[0].toCharsString();
+			return signatures;
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
